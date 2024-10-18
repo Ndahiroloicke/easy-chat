6d2ec53e67968222954e15ed6f975496f1618d53
Ndahiroloicke <ndahiroloicke@gmail.com>
2024-10-18 10:50:39 +0200

    collaborative edition

--- utils/firebase.config.ts
+++ utils/firebase.config.ts
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your Firebase configuration object
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCsAj9VVmA2kB7MzO4yV1DSv6Yuh77JErg",
-  authDomain: "launez-54b3a.firebaseapp.com",
-  projectId: "launez-54b3a",
-  storageBucket: "launez-54b3a.appspot.com",
-  messagingSenderId: "881274176774",
-  appId: "1:881274176774:web:7bae223e17fd7b7c3f0a87"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
