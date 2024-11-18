ea2247d591049cba1bf0e5a165a2e4c67c1e1a14
Ndahiroloicke <ndahiroloicke@gmail.com>
2024-11-18 12:58:01 +0200

    development

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig =  {
-    apiKey: "AIzeSbSe819VWm2k879PzD4vV1DSvGVbD7ZJErg",
-    authDomain: "launez-54b3a.firebaseapp.com",
-    projectId: "launez-54b3a",
-    storageBucket: "launez-54b3a.appspot.com",
-    messagingSenderId: "881274176774",
-    appId: "1:881274176774:web:7bae223e17fd7b7c3f0a87"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCsAj9VVmA2kB7MzO4yV1DSv6Yuh77JErg",
+  authDomain: "launez-54b3a.firebaseapp.com",
+  projectId: "launez-54b3a",
+  storageBucket: "launez-54b3a.appspot.com",
+  messagingSenderId: "881274176774",
+  appId: "1:881274176774:web:7bae223e17fd7b7c3f0a87"
+};
 
 export default firebaseConfig; 
